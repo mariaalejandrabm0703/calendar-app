04835d58edf0bccd739e11f2f3f6a453473d6972
Maria Bohorquez <mariaalejandrabm0703@gmail.com>
2021-03-23 22:50:28 -0500

    Navbar.

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Navbar } from '../ui/Navbar'
 
 export const CalendarScreen = () => {
     return (
         <>
-            <h1>Hola CalendarScreen</h1>
+            <Navbar/>
         </>
     )
 }
